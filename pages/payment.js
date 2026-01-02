72ea0c61d5c4bfe9c6058d4a042ae2fabea53993
roshicreates <svroshini7@gmail.com>
2026-01-03 00:11:46 +0530

    Update and rename pages/pages/pages/payment.js to pages/payment.js

--- pages/payment.js
+++ pages/payment.js
@@ -1,5 +1,5 @@
 export default function Payment() {
-  return (
+  return (https://github.com/roshicreates/birthday-scrapbook/tree/main/pages
     <div style={{ padding: 40 }}>
       <h2>Payment Page</h2>
       <p>Pay to unlock scrapbook creation</p>
